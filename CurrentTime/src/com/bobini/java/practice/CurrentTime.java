4beda36a44ba4d19009ab2af6d9afc05c3d0142c
bobinii <kimjini9030@gmail.com>
2023-02-02 21:32:28 -0700

    Completed CurrentTime class

---
+++ CurrentTime/src/com/bobini/java/practice/CurrentTime.java
@@ -0,0 +1,10 @@
+package com.bobini.java.practice;
+
+import java.util.Date;
+
+public class CurrentTime {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
