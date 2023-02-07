a21e89a9c44192458495e7ecd6d3efcde05701d7
bobinii <kimjini9030@gmail.com>
2023-02-06 23:57:44 -0700

    Completed Ages class

---
+++ src/com/bobini/java/practice/Ages.java
@@ -0,0 +1,11 @@
+package com.bobini.java.practice;
+
+public class Ages {
+    public static void main(String[] args) {
+        int bobiniAge = 27;
+        int boboyfriendAge = 29;
+        String answer = (bobiniAge < boboyfriendAge)? "yes" : "no";
+        System.out.println("is bobini's boyfriend older than bobini?");
+        System.out.println("the answer is " + answer);
+    }
+}
