e0bb4e49ff5977b897a68df7914111e456908673
jinic808 <kimjini9030@gmail.com>
2023-03-18 00:41:34 -0700

    Finished cleaning up for MortgageCalculator

--- src/com/jinic/java/practice/MortgageCalculator.java
+++ src/com/jinic/java/practice/MortgageCalculator.java
@@ -5,45 +5,35 @@ import java.util.Scanner;
 
 public class MortgageCalculator {
     public static void main(String[] args) {
+        int principal = (int)readInput("Principal", 1000, 1_000_000);
+        float annualInterest = (float)readInput("Annual interest rate", 0, 30);
+        float years = (float)readInput("Period", 1, 30);
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+    }
+
+    public static double calculateMortgage(int principal, float annualInterest, float years) {
         final byte PERCENT = 100;
         final byte MONTHS_OF_A_YEAR = 12;
 
-        int principal;
-        float monthlyInterest;
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_OF_A_YEAR;
+        float period = years * MONTHS_OF_A_YEAR;
 
+        float pow = (float)Math.pow(1 + monthlyInterest, period);
+
+        return (principal * monthlyInterest * pow) / (pow - 1);
+    }
+
+    public static double readInput(String value, int min, int max) {
         Scanner scanner = new Scanner(System.in);
+        double result;
         while (true) {
-            System.out.print("Principal ($1K - $1M): ");
-            principal = scanner.nextInt();
-            if (principal >= 1000 && principal <= 1000000) {
-                break;
+            System.out.print(value + "(" + min + " - " + max + "): ");
+            result = scanner.nextFloat();
+            if (result >= min && result <= max) {
+                return result;
             } else {
-                System.out.println("Enter a number between 1,000 to 1,000,000");
-            }
-        }
-
-        while (true) {
-            System.out.print("Annual interest rate (0 - 30): ");
-            float annualInterest = scanner.nextFloat();
-            monthlyInterest = annualInterest / PERCENT / MONTHS_OF_A_YEAR;
-            if (annualInterest > 0 && annualInterest <= 30) {
-                break;
-            } else {
-                System.out.println("Enter a value greater than 0 and less than or equal to 30");
-            }
-        }
-
-        while (true) {
-            System.out.print("Period (1 - 30): ");
-            float years = scanner.nextFloat();
-            if (years >= 1 && years <= 30) {
-                float period = years * MONTHS_OF_A_YEAR;
-                float pow = (float)Math.pow(1 + monthlyInterest, period);
-                double mortgage = (principal * monthlyInterest * pow) / (pow - 1);
-                System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
-                break;
-            } else {
-                System.out.println("Enter a value between 1 and 30");
+                System.out.println("Enter a value between " + min + " and " + max);
             }
         }
     }
