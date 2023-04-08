d084aa4cead2765fd9851b32a0e1452dda21c6c6
jinic808 <kimjini9030@gmail.com>
2023-04-07 19:24:43 -0700

    Removed files from original dir

--- src/com/jinic/java/practice/AConstant.java
+++
@@ -1,8 +0,0 @@
-package com.jinic.java.practice;
-
-public class AConstant {
-    public static void main(String[] args) {
-        final String MY_LOVE = "bobini's boyfriend";
-        System.out.println("bobini loves " + MY_LOVE + " and " + MY_LOVE + " loves bobini >3<");
-    }
-}
--- src/com/jinic/java/practice/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.jinic.java.practice;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World from bobini");
-    }
-}
